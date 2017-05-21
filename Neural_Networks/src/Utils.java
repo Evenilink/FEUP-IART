d1f6f62e56dbfb27161b5f9b2f76ca2ae7cc2629
Evenilink <jose__carlos17@hotmail.com>
2017-05-21 21:29:19 +0100

    Using array, since on the GitHub we're using neuroph v.2.92

--- Neural_Networks/src/Utils.java
+++ Neural_Networks/src/Utils.java
@@ -9,15 +9,5 @@ public class Utils {
     public static final String PERFORMANCE_FOLDER = "data/performance/";
     public static final String LEARNING_ERROR_FOLDER = "data/learning_error/";
 
-    public static final int HORIZONTAL_RESOLUTION = 640;
-    public static final int VERTICAL_RESOLUTION = 480;
-    public static final int MAX_DEEPTH = 4000;
-
-    public static final int DEFAULT_MAX_ITERATIONS = 5000;
-    public static final double DEFAULT_MAX_ERROR = 0.0007;
-    public static final float LEARNING_RATE = 0.01f;
-    public static final int DEFAULT_NUM_HIDDEN_LAYERS = 5;
-
-    public static final float LEARNING_RATE_INCREMENT = 0.2f;
     public static final int PERCENTAGE_TO_TRAIN = 80;
 }
